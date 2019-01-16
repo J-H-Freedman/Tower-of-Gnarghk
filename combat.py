af3552108e2970b0fe1027cf7468858fd719b2ba
J-H-Freedman <37190247+J-H-Freedman@users.noreply.github.com>
2019-01-16 12:21:50 -0500

    updated combat.py and dice.py
    cleaned up the code to show my better understanding of OOP

--- combat.py
+++ combat.py
@@ -1,35 +1,59 @@
 from dice import *
 
-def Strike(equipment):
-	'''calculate damage by using a multiplyer
-	and then distribute it between the target and body'''
-	roll_damage = Fudge().RollAllDice()
-	roll_aim = d6().RollAllDice()
-#	TEST = 100
-#	if equipment == TEST:
-#		for i in range(4, -5, -1):
-#			roll_damage = i
-#			for i in range(1, 7):
-#				roll_aim = i
-#				print(d6().Distribution(roll_aim, Fudge().Multiplier(equipment, roll_damage)))
-	target_damage_from_strike, body_damage_from_strike = d6().Distribution(
-		roll_aim, Fudge().Multiplier(equipment, roll_damage)
-		)
-	return target_damage_from_strike, body_damage_from_strike
+# There's got to be a way to import instances of objects in other files...
+# Maybe create classes for each?
+FOURdF = Dice(4, [-1, -1, 0, 0, 1, 1])
+ONEd6 = Dice(1, [1, 2, 3, 4, 5, 6])
+
+class Attack(object):
+	def _MultiplyDamage(equipment, total_from_dice):
+		'''Calculate total damage based on the player's weapon times a multiplier dictated by 4dF'''
+		multiplier = math.sin(total_from_dice / 3) + 1
+		if total_from_dice >= 4:
+			return equipment * 3
+			# you take a bonus turn
+		elif total_from_dice <= -4:
+			return 0
+			# enemy takes a bonus turn
+		else:
+			return equipment * multiplier
+
+	def _DistributeDamage(distribution_value, damage):
+		'''Calculate where the damage goes based on the aim of 1d6'''
+		damage_to_target = damage * ((distribution_value - 1) / 5) # find a way account for warped sides of the d6
+		damage_to_body = damage - damage_to_target
+		return int(damage_to_target), int(damage_to_body)
+			#if distribution = 6:
+			#	take another attack
+
+	def Strike(equipment):
+		'''calculate damage by using a multiplyer and then distribute it between the target and body'''
+		roll_damage = FOURdF.RollAllDice()
+		roll_aim = ONEd6.RollAllDice()
+		damage_to_target, damage_to_body =  Attack._DistributeDamage(roll_aim, Attack._MultiplyDamage(equipment, roll_damage))
+		return damage_to_target, damage_to_body
 
 if __name__ == "__main__":
 	def Main():
 		sword = 100
 
-		'''testing strike with printed explanation'''
-		target_damage_from_strike, body_damage_from_strike = Strike(sword)
-		print(
-			"damage to target is:", target_damage_from_strike,
-			"\n damage to body is:", body_damage_from_strike
-			)
+		# to test damage multiplier
+		for i in range(4, -5, -1): 
+			print("damage is: ", Attack._MultiplyDamage(sword, i))
 
-#		TEST = 100
-#		Strike(TEST)		
+		# to test damage distribution
+		for i in range(1, 7): 
+			damage_to_target, damage_to_body = Attack._DistributeDamage(i, sword)
+			print("damage to target is:", damage_to_target, "\n damage to body is:", damage_to_body)
+
+		'''testing strike with printed explanation'''
+		for i in range(10):
+			damage_to_target, damage_to_body = Attack.Strike(sword)
+			print(
+				"damage to target is:", damage_to_target,
+				"\n damage to body is:", damage_to_body
+				)
+		
 		input("continue?")
 
 	Main()
--- dice.py
+++ dice.py
@@ -1,13 +1,13 @@
 import random
 import math
 
+# Create Dice
 class Dice(object):
-	def __init__(self, number, sides, probability):
-		self.number = number
-		self.sides = sides
-#		self.probability = probability
+	def __init__(self, number, sides):
+		self._number = number
+		self._sides = sides
 
-	def RollDie(self):
+	def _RollDie(self):
 		return random.choice(self._sides)
 
 	def RollAllDice(self):
@@ -15,70 +15,22 @@ class Dice(object):
 		total_from_dice = 0
 
 		for i in range(self._number):			
-			total_from_dice += self.RollDie()
+			total_from_dice += self._RollDie()
 
 		return total_from_dice
 
-class Fudge(Dice):
-	def __init__(self):
-		self.number = 4
-		self.sides = [-1, -1, 0, 0, 1, 1]
-#		self.__probability__ = {
-#			4 : 1/81,
-#			3 : 4/81,
-#			2 : 10/81,
-#			1 : 16/81,
-#			0 : 19/81,
-#			-1 : 16/81,
-#			-2 : 10/81,
-#			-3 : 4/81,
-#			-4 : 1/81
-#		}
+# Commonly Used dice
+FOURdF = Dice(4, [-1, -1, 0, 0, 1, 1])
+ONEd6 = Dice(1, [1, 2, 3, 4, 5, 6])
 
-	def Multiplier(self, equipment, total_from_dice):
-		'''Calculate total damage based on the player's weapon times a multiplier dictated by 4dF'''
-		multiplier = math.sin(total_from_dice / 3) + 1
-		if total_from_dice == 4:
-			return equipment * 3
-			# you take a bonus turn
-		elif total_from_dice == -4:
-			return 0
-			# enemy takes a bonus turn
-		else:
-			return equipment * multiplier
-
-class d6(Dice):
-	def __init__(self):
-		self.number = 1
-		self.sides = [1, 2, 3, 4, 5, 6]
-#		self.__probability__ = {
-#			1 : (1/6), 
-#			2 : (1/6), 
-#			3 : (1/6), 
-#			4 : (1/6), 
-#			5 : (1/6), 
-#			6 : (1/6)
-#		}
-
-	def Distribution(self, distribution_value, damage):
-		'''Calculate where the damage goes based on the aim of 1d6'''
-		damage_to_target = damage * ((distribution_value - 1) / 5)
-		damage_to_body = damage - damage_to_target
-		return int(damage_to_target), int(damage_to_body)
-			#if distribution = 6:
-			#	take another attack
-
+# Testing...
 if __name__ == "__main__":
 	def Main():
 		sword = 100
 
-		# to test damage multiplier
-		for i in range(4, -5, -1): 
-			print("damage is: ", Fudge().Multiplier(sword, i))
-
-		# to test damage distribution
-		for i in range(1, 7): 
-			damage_to_target, damage_to_body = d6().Distribution(i, sword)
-			print("damage to target is:", damage_to_target, "\n damage to body is:", damage_to_body)
+		# to test dice
+		for i in range(10):
+			print("The F roll is", FOURdF.RollAllDice())
+			print("The d6 roll is", ONEd6.RollAllDice())
 
 	Main()
