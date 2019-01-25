5e71c38cdb90fea7157ecf069a94a28fac593400
J-H-Freedman <37190247+J-H-Freedman@users.noreply.github.com>
2019-01-25 00:02:16 -0500

    Clean up code on dice.py and combat.py
    improved oop concepts

--- combat.py
+++ combat.py
@@ -1,59 +1,75 @@
 from dice import *
 
-# There's got to be a way to import instances of objects in other files...
-# Maybe create classes for each?
-FOURdF = Dice(4, [-1, -1, 0, 0, 1, 1])
-ONEd6 = Dice(1, [1, 2, 3, 4, 5, 6])
+# Lots of dead variables.  Create an equipment file.
 
-class Attack(object):
-	def _MultiplyDamage(equipment, total_from_dice):
+class Damage:
+	def _RollDamage(self, weapon_dice):
+		return weapon_dice.Roll()
+
+	def Multiplier(self, damage_result, multiplier_result):
 		'''Calculate total damage based on the player's weapon times a multiplier dictated by 4dF'''
-		multiplier = math.sin(total_from_dice / 3) + 1
-		if total_from_dice >= 4:
-			return equipment * 3
+		multiplier = math.sin(multiplier_result / 3) + 1
+		if multiplier_result >= 4:
+			return damage_result * 3
 			# you take a bonus turn
-		elif total_from_dice <= -4:
+		elif multiplier_result <= -4:
 			return 0
 			# enemy takes a bonus turn
 		else:
-			return equipment * multiplier
+			return damage_result * multiplier
 
-	def _DistributeDamage(distribution_value, damage):
+	def Distribute(self, distribution_result, multiplied_damage):
 		'''Calculate where the damage goes based on the aim of 1d6'''
-		damage_to_target = damage * ((distribution_value - 1) / 5) # find a way account for warped sides of the d6
-		damage_to_body = damage - damage_to_target
-		return int(damage_to_target), int(damage_to_body)
+		damage_to_target = multiplied_damage * ((distribution_result - 1) / 5) # find a way account for warped sides of the d6
+		excess_damage = multiplied_damage - damage_to_target
+		return int(damage_to_target), int(excess_damage)
 			#if distribution = 6:
 			#	take another attack
 
-	def Strike(equipment):
+class SuccessfulAttack:
+	def _CalcualteBlocked(self, damage_result):
 		'''calculate damage by using a multiplyer and then distribute it between the target and body'''
-		roll_damage = FOURdF.RollAllDice()
-		roll_aim = ONEd6.RollAllDice()
-		damage_to_target, damage_to_body =  Attack._DistributeDamage(roll_aim, Attack._MultiplyDamage(equipment, roll_damage))
-		return damage_to_target, damage_to_body
+		damage_to_arm, damage_to_shield = Damage.Distribute(roll_aim, self._CalculateStandard(damage_result))
+		return damage_to_arm, damage_to_shield
 
+	def _CalculateStandard(self, damage_result):
+		return Damage.Multiplier(damage_result, roll_damage)
+
+class AimWeapon:
+	def _Calculate(self, aim_threshold, aim_result):
+		if aim_result > aim_threshold:
+			return aim == True
+		elif aim_result == aim_threshold:
+			return glancing_blow == True
+		else:
+			return aim == False
+
+	def GetAccuracy(self, equipment):
+		return self._Calculate(equipment.HitChance, AccuracyDie.Roll())
+
+'''
 if __name__ == "__main__":
-	def Main():
-		sword = 100
+	sword = Dice(10, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+	FOURdF = Dice(4, [-1, -1, 0, 0, 1, 1])
+	ONEd6 = Dice(1, [1, 2, 3, 4, 5, 6])
 
-		# to test damage multiplier
-		for i in range(4, -5, -1): 
-			print("damage is: ", Attack._MultiplyDamage(sword, i))
+	# to test damage multiplier
+	for i in range(4, -5, -1): 
+		print("damage is: ", Damage._Multiplier(sword, i))
 
-		# to test damage distribution
-		for i in range(1, 7): 
-			damage_to_target, damage_to_body = Attack._DistributeDamage(i, sword)
-			print("damage to target is:", damage_to_target, "\n damage to body is:", damage_to_body)
+	# to test damage distribution
+	for i in range(1, 7): 
+		damage_to_target, damage_to_body = Damage.Distribute(i, sword)
+		print("damage to target is:", damage_to_target, "\n damage to body is:", damage_to_body)
 
-		'''testing strike with printed explanation'''
-		for i in range(10):
-			damage_to_target, damage_to_body = Attack.Strike(sword)
-			print(
-				"damage to target is:", damage_to_target,
-				"\n damage to body is:", damage_to_body
-				)
-		
-		input("continue?")
+	#testing strike with printed explanation
+	for i in range(10):
+		damage_to_target, damage_to_body = Damage.Strike(sword)
+		print(
+			"damage to target is:", damage_to_target,
+			"\n damage to body is:", damage_to_body
+			)
+	
+	input("continue?")
 
-	Main()
+	'''
--- dice.py
+++ dice.py
@@ -2,35 +2,72 @@ import random
 import math
 
 # Create Dice
-class Dice(object):
-	def __init__(self, number, sides):
-		self._number = number
-		self._sides = sides
+class Die:
+	def __init__(self, die_sides):
+		self.die_sides = die_sides
 
-	def _RollDie(self):
-		return random.choice(self._sides)
+	@staticmethod
+	def Generator(number_of_sides):
+		'''Generate die based on the number of sides'''
+		return [i for i in range(1, number_of_sides + 1)]
 
-	def RollAllDice(self):
-		''' Standard dice roll'''
+	def Roll(self):
+		'''Get random side from die'''
+		return random.choice(die_sides)
+
+	def GetDie(self):
+		return self.die_sides
+
+	def AddSide(self, side):
+		self.die_sides.append(side)
+
+	def RemoveSide(self, side):
+		self.die_sides.remove(side)
+
+class Dice:
+	def __init__(self, number_of_dice, die_type):
+		self._number_of_dice = number_of_dice
+		self._die_type = die_type
+
+	@staticmethod
+	def Generator(self, number_of_dice, number_of_sides):
+		'''Generate dice based on the number of sides'''
+		return number_of_dice, Die.Generator(number_of_sides)
+
+	def Roll(self):
+		'''Add random sides from dice'''
+		GetDie(die_type)
+
 		total_from_dice = 0
 
-		for i in range(self._number):			
-			total_from_dice += self._RollDie()
+		for i in range(self._number_of_dice):			
+			total_from_dice += die_type.Roll()
 
 		return total_from_dice
 
-# Commonly Used dice
-FOURdF = Dice(4, [-1, -1, 0, 0, 1, 1])
-ONEd6 = Dice(1, [1, 2, 3, 4, 5, 6])
+	def GetAllDice(self):
+		return self._number_of_dice, self._die_type
 
+	def ChangeDiceAmount(self, new_amount):
+		return self._number_of_dice = new_amount
+
+	def ChangeDiceType(self, new_type):
+		return self._die_type = new_type
+
+'''
 # Testing...
+# fix main
 if __name__ == "__main__":
-	def Main():
-		sword = 100
+	sword = 100
 
-		# to test dice
-		for i in range(10):
-			print("The F roll is", FOURdF.RollAllDice())
-			print("The d6 roll is", ONEd6.RollAllDice())
+	# Commonly Used dice
+	dF = Die([-1, -1, 0, 0, 1, 1])
+	d6 = Die([1, 2, 3, 4, 5, 6])
+	FOURdF = Dice(4, dF)
+	ONEd6 = Dice(1, d6)
 
-	Main()
+	# to test dice
+	for i in range(10):
+		print("The F roll is", FOURdF.Roll())
+		print("The d6 roll is", ONEd6.Roll())
+		'''
